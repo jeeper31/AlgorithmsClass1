0579607719e7c8c26ce3a60b89f4badc532371c9
jeeper31 <alex.gaddie@gmail.com>
2013-01-30 18:53:43 -0600

    Initial commit, the damn thing doesnt work yet, but its close.

---
+++ Algorithms1/src/com/gaddieind/mergesort/MergeSort.groovy
@@ -0,0 +1,46 @@
+package com.gaddieind.mergesort
+
+class MergeSort {
+	
+	public static void main(String[] args){
+		List myList = [8,3,4,2,5,6,1,7,9,0]
+		List sorted = sort(myList)
+		
+		assert sorted == myList.sort()
+	}
+	
+	
+	private static List sort(List someList){
+		if(someList.size() == 1) return someList
+		
+		List finalList = new ArrayList()
+		int half = someList.size() / 2
+		List left = sort(someList.subList(0, half))
+		List right = sort(someList.subList(half, someList.size()))
+		int i, j = 0
+		
+		for(int k = 0; k < someList.size(); k++){
+			if(i == half || i == right.size()){
+				finalList.add(k, right.get(j))
+				j++
+				continue
+			}
+			
+			if(j == half || j == left.size()){
+				finalList.add(k, left.get(i))
+				i++
+				continue
+			}
+			
+			if(left.get(i) < right.get(j)){
+				finalList.add(k, left.get(i))
+				i++
+			}else{
+				finalList.add(k, right.get(j))
+				j++
+			}
+		}
+		
+		return finalList
+	}
+}
